e19aa237497a2e9990bee4d0b489625452af958d
pavelsokolovan <pavel.sokolov.an@gmail.com>
2015-05-29 15:55:10 +0300

    first commit

---
+++ MiltCalculator/ISimpleCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace MiltCalculator
+{
+    [ServiceContract]
+    public interface ISimpleCalculator
+    {
+        [OperationContract]
+        double Plus(double firstNumber, double secondNumber);
+        [OperationContract]
+        double Minus(double firstNumber, double secondNumber);
+        [OperationContract]
+        double Multiply(double firstNumber, double secondNumber);
+        [OperationContract]
+        double Divide(double firstNumber, double secondNumber);
+    }
+
+    [ServiceContract]
+    public interface IAdvancedCalculator
+    {
+        [OperationContract]
+        double SquareRoot(double number);
+        [OperationContract]
+        double Power(double number, double power);
+    }
+}
